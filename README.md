cba0ae8e58d13663883c3b0948f6f7d373c58ca4
Peanut <michjmend@gmail.com>
2019-03-16 08:51:29 -0400

    okay now this is the final commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # unit-4-game
 jQuery game
+<!-- describe the project.  -->
+
+1. This project is a game that we have to make using jQuery. The goal of the game is to add up random numbers to equal the random target number that the computer generates.
+2. This project is great practice for those using bootstrap grid and or jQuery.
+3. You can get started by first creating your html and folders needed for the entire project. Here are some steps that might make things a little more organized.
+   1. Pseudocode the entire process first. This might actually save you some times later down the line
+   2. Don't forget link the css and javascript files to your html page! (bootstrap also)
+   3. Once you finish your html, javascript next! Console logging is a big help to catch mistakes.
+   4. Last, make it pretty with css!
+
+tips that I need to remember:
+-EACH crystal needs a math random function so that it'll change its value after every win or loss.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,93 @@
 $(document).ready(function() {
-
+ //set variables
   var wins = 0;
   var losses = 0;
   var counter = 0;
-
-  var targetNumber =  parseInt(Math.floor(Math.random() * 120) + 19);
+//set target number variable such that the computer can generate a random number.
+  var targetNumber =  (Math.floor(Math.random() * 120) + 19);
+//use html functions to post values directly in the DOM so it appears in the browser
   $("#randomNumber").html(targetNumber);
-  console.log("targetNumber" + targetNumber);
-
-  var crystal1 = parseInt(Math.floor(Math.random() * 12) + 1);
-  var crystal2 = parseInt(Math.floor(Math.random() * 12) + 1);
-  var crystal3 = parseInt(Math.floor(Math.random() * 12) + 1);
-  var crystal4 = parseInt(Math.floor(Math.random() * 12) + 1);
-  console.log("crystal1" + crystal1)
-  console.log("crystal2" + crystal2)
-  console.log("crystal3" + crystal3)
-  console.log("crystal4" + crystal4)
-
+  $("winsText").html(wins);
+  $("lossesText").html(losses);
+//create variables for EACH crystal such that the computer generates random values for each crsytal after each win or loss.
+  var crystal1 = (Math.floor(Math.random() * 12) + 1);
+  var crystal2 = (Math.floor(Math.random() * 12) + 1);
+  var crystal3 = (Math.floor(Math.random() * 12) + 1);
+  var crystal4 = (Math.floor(Math.random() * 12) + 1);
+//now set the rest of the code to perform the functions needed for the game to work.
   function reset () {
     counter = 0;
     $("#counter").html(counter);
-    targetNumber =  parseInt(Math.floor(Math.random() * 120) + 19);
+    targetNumber =  (Math.floor(Math.random() * 120) + 19);
     $("#randomNumber").html(targetNumber);
-
-    crystal1 = parseInt(Math.floor(Math.random() * 12) + 1);
-    crystal2 = parseInt(Math.floor(Math.random() * 12) + 1);
-    crystal3 = parseInt(Math.floor(Math.random() * 12) + 1);
-    crystal4 = parseInt(Math.floor(Math.random() * 12) + 1);
+//this function will create a reset after each win/loss has been reached.
+    crystal1 = (Math.floor(Math.random() * 12) + 1);
+    crystal2 = (Math.floor(Math.random() * 12) + 1);
+    crystal3 = (Math.floor(Math.random() * 12) + 1);
+    crystal4 = (Math.floor(Math.random() * 12) + 1);
   }
-
+//I found a checkifwon function that will read my if statement and see if the values match completely. I called the function again at the end so that it could record the result.
   $("#crystal1").on("click", function() {
 
     counter = counter + crystal1;
     $("#counter").html(counter);
-    console.log("counter1" + counter);
 
     if (counter == targetNumber) {
         checkifwon();
     } else if (counter > targetNumber) {
         checkifwon();
     }
+
   });
 
   $("#crystal2").on("click", function() {
 
     counter = counter + crystal2;
     $("#counter").html(counter);
-    console.log("counter2" + counter);
 
     if (counter == targetNumber) {
         checkifwon();
     } else if (counter > targetNumber) {
         checkifwon();
     }
+
   });
 
   $("#crystal3").on("click", function() {
 
     counter = counter + crystal3;
     $("#counter").html(counter);
-    console.log("counter3" + counter);
 
     if (counter == targetNumber) {
         checkifwon();
     } else if (counter > targetNumber) {
         checkifwon();
     }
+
   });
 
   $("#crystal4").on("click", function() {
 
     counter = counter + crystal4;
     $("#counter").html(counter);
-    console.log("counter4" + counter);
 
     if (counter == targetNumber) {
         checkifwon();
     } else if (counter > targetNumber) {
         checkifwon();
     }
+
   });
 
-  // create win and lost
   function checkifwon() {
 
     if(counter === targetNumber) {
     wins++;
-    console.log(wins);
     $("#result").html("You won!!");
     $("#winsText").html("Wins: " + wins);
     reset();
     } else if (counter > targetNumber) {
     losses++;
-    console.log(losses);
     $("#result").html("You lost!!");
     $("#lossesText").html("Losses: " + losses);
     reset();
